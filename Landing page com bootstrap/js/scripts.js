24d2642c2a7d433f91eaf60d30b90f60e2700284
vitoriaazevedo16 <vitoriaregia@being.com.br>
2024-09-23 16:56:19 -0300

    Landinpage com bootstrap
    Com carrossel, galeria e acordeon incluido.

---
+++ Landing page com bootstrap/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   
+    $('.gallery-001').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{enabled:true}
+        // other options
+      });
+  });
+  
